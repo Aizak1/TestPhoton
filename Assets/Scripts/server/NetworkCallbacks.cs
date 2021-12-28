59a78b7b82af3aa285fe1085c62646d095adab82
AlexanderKravchenkoS <kravchenko.alexander.s@gmail.com>
2021-12-28 12:58:55 +0300

    Spawn player

---
+++ Assets/Scripts/server/NetworkCallbacks.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Photon.Bolt;
+
+public class NetworkCallbacks : GlobalEventListener
+{
+    [SerializeField] private float _minX;
+    [SerializeField] private float _maxX;
+    [SerializeField] private float _minY;
+    [SerializeField] private float _maxY;
+
+    [SerializeField] Player _player;
+
+    public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+    {
+        float x = Random.Range(_minX, _maxX);
+        float y = Random.Range(_minY, _maxY);
+
+        BoltNetwork.Instantiate(_player.gameObject, new Vector2(x,y), Quaternion.identity);
+    }
+}
